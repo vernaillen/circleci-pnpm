4b9ea0aa4cd334cdfc4e32a1553683eb6fdeab5a
Wouter Vernaillen <wouter@vernaillen.com>
2022-12-13 07:11:44 +0100

    cimg/node:19.2

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM cimg/node:19.1
+FROM cimg/node:19.2
 
 RUN sudo npm i -g pnpm
 RUN sudo node - add --global pnpm
--- build-images.sh
+++ build-images.sh
@@ -1,2 +1,2 @@
-docker build --file Dockerfile -t vernaillen/circleci-pnpm:19.1.0 -t vernaillen/circleci-pnpm:19.1 .
-docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:19.1.0-browsers -t vernaillen/circleci-pnpm:19.1-browsers .
+docker build --file Dockerfile -t vernaillen/circleci-pnpm:19.2.0 -t vernaillen/circleci-pnpm:19.2 .
+docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:19.2.0-browsers -t vernaillen/circleci-pnpm:19.2-browsers .
