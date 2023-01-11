6097093b6720db3a1aea19ca73d64581902de6a4
Wouter Vernaillen <wouter@vernaillen.com>
2023-01-11 08:34:09 +0100

    update scripts for 19.3

--- build-images.sh
+++ build-images.sh
@@ -1,2 +1,2 @@
-docker build --file Dockerfile -t vernaillen/circleci-pnpm:19.2.0 -t vernaillen/circleci-pnpm:19.2 .
-docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:19.2.0-browsers -t vernaillen/circleci-pnpm:19.2-browsers .
+docker build --file Dockerfile -t vernaillen/circleci-pnpm:19.3.0 -t vernaillen/circleci-pnpm:19.3 .
+docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:19.3.0-browsers -t vernaillen/circleci-pnpm:19.3-browsers .
