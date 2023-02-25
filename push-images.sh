bcccc032cef2812e24e06070eeb574ad7c9edcc0
Wouter Vernaillen <wouter@vernaillen.com>
2023-02-25 14:34:43 +0100

    node:19.7

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
-docker push vernaillen/circleci-pnpm:19.5
-docker push vernaillen/circleci-pnpm:19.5.0
-docker tag  vernaillen/circleci-pnpm:19.5.0 vernaillen/circleci-pnpm:current
+docker push vernaillen/circleci-pnpm:19.7
+docker push vernaillen/circleci-pnpm:19.7.0
+docker tag  vernaillen/circleci-pnpm:19.7.0 vernaillen/circleci-pnpm:current
 docker push vernaillen/circleci-pnpm:current
 
-docker push vernaillen/circleci-pnpm:19.5-browsers
-docker push vernaillen/circleci-pnpm:19.5.0-browsers
-docker tag  vernaillen/circleci-pnpm:19.5.0-browsers vernaillen/circleci-pnpm:current-browsers
+docker push vernaillen/circleci-pnpm:19.7-browsers
+docker push vernaillen/circleci-pnpm:19.7.0-browsers
+docker tag  vernaillen/circleci-pnpm:19.7.0-browsers vernaillen/circleci-pnpm:current-browsers
 docker push vernaillen/circleci-pnpm:current-browsers
 
