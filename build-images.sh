af5f8dda919cbf5aa49d3f04963159828d967720
Wouter Vernaillen <wouter@vernaillen.com>
2023-05-06 06:11:16 +0200

    build & push v20.1

--- build-images.sh
+++ build-images.sh
@@ -1,2 +1,2 @@
-docker build --file Dockerfile -t vernaillen/circleci-pnpm:20.0.0 -t vernaillen/circleci-pnpm:20.0 .
-docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:20.0.0-browsers -t vernaillen/circleci-pnpm:20.0-browsers .
+docker build --file Dockerfile -t vernaillen/circleci-pnpm:20.1.0 -t vernaillen/circleci-pnpm:20.1 .
+docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:20.1.0-browsers -t vernaillen/circleci-pnpm:20.1-browsers .
