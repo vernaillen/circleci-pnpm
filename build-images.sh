048997d554eaef6d83d014eeead9ff845f9fb89a
Wouter Vernaillen <wouter@vernaillen.com>
2023-06-21 10:24:20 +0200

    20.3.1

--- build-images.sh
+++ build-images.sh
@@ -1,2 +1,2 @@
-docker build --file Dockerfile -t vernaillen/circleci-pnpm:20.2.0 -t vernaillen/circleci-pnpm:20.2 .
-docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:20.2.0-browsers -t vernaillen/circleci-pnpm:20.2-browsers .
+docker build --file Dockerfile -t vernaillen/circleci-pnpm:20.3.1 -t vernaillen/circleci-pnpm:20.3 .
+docker build --file browsers/Dockerfile -t vernaillen/circleci-pnpm:20.3.1-browsers -t vernaillen/circleci-pnpm:20.3-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
-docker push vernaillen/circleci-pnpm:20.2
-docker push vernaillen/circleci-pnpm:20.2.0
-docker tag  vernaillen/circleci-pnpm:20.2.0 vernaillen/circleci-pnpm:current
+docker push vernaillen/circleci-pnpm:20.3
+docker push vernaillen/circleci-pnpm:20.3.1
+docker tag  vernaillen/circleci-pnpm:20.3.1 vernaillen/circleci-pnpm:current
 docker push vernaillen/circleci-pnpm:current
 
-docker push vernaillen/circleci-pnpm:20.2-browsers
-docker push vernaillen/circleci-pnpm:20.2.0-browsers
-docker tag  vernaillen/circleci-pnpm:20.2.0-browsers vernaillen/circleci-pnpm:current-browsers
+docker push vernaillen/circleci-pnpm:20.3-browsers
+docker push vernaillen/circleci-pnpm:20.3.1-browsers
+docker tag  vernaillen/circleci-pnpm:20.3.1-browsers vernaillen/circleci-pnpm:current-browsers
 docker push vernaillen/circleci-pnpm:current-browsers
